526c3b7df700d473bd971f336f7edd6d316e52bd
Peleshko <berdyanskpel6@gmail.com>
2021-10-12 21:01:25 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ import CountdownTimer from './timer.js';
 
 const timer = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Oct 12, 2021, 21:01:00'),
+  targetDate: new Date('Dec 31, 2021'),
 });
 
 timer.start();
